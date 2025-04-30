0c0cd38a55171976176fa8ec6cf3519de4ac0f5b
root <root@orik.umar.kz>
2025-04-30 13:54:46 +0500

    serv

--- talant_school/talant_school/settings.py
+++ talant_school/talant_school/settings.py
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Путь, куда Django будет собирать все static-файлы
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Папки, откуда Django будет забирать исходную статику
+STATICFILES_DIRS = [BASE_DIR / 'talant_app/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -125,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+CSRF_TRUSTED_ORIGINS = [
+    "https://talant.inbrain.kz",
+]
